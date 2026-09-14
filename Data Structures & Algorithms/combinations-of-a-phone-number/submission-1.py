2608dbf69cbf71d8df4f7f3fa11077e7c9a2a003
Shupeng Lin <126983148+KLin06@users.noreply.github.com>
2026-09-13 22:11:25 -0400

    Add: combinations-of-a-phone-number - submission-1

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-1.py
@@ -0,0 +1,35 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        n = len(digits)
+        phone_keypad = {
+            '2': ['a', 'b', 'c'],
+            '3': ['d', 'e', 'f'],
+            '4': ['g', 'h', 'i'],
+            '5': ['j', 'k', 'l'],
+            '6': ['m', 'n', 'o'],
+            '7': ['p', 'q', 'r', 's'],
+            '8': ['t', 'u', 'v'],
+            '9': ['w', 'x', 'y', 'z'],
+        }
+
+        sol = []
+        def backtrack(digits, curr, i):
+            print(curr)
+            nonlocal sol
+            if i == n: 
+                if curr:
+                    sol.append("".join(curr))
+                return 
+
+            for c in phone_keypad[digits[i]]:
+                curr.append(c)
+                backtrack(digits, curr, i + 1)
+                curr.pop()
+
+        backtrack(digits, [], 0)
+
+        return sol
+
+
+        
+        
