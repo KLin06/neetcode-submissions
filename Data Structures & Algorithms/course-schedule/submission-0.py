b046e165e5f1c302929b2dd229b245a3ddd9fdb6
Shupeng Lin <126983148+KLin06@users.noreply.github.com>
2026-09-17 09:38:48 -0400

    Add: course-schedule - submission-0

---
+++ Data Structures & Algorithms/course-schedule/submission-0.py
@@ -0,0 +1,27 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        prereqs = defaultdict(list)
+        num_prereqs = [0] * numCourses
+
+        for a,b in prerequisites:
+            # b is a prereq for a
+            prereqs[b].append(a)
+            num_prereqs[a] += 1
+        
+        print(num_prereqs)
+        
+        stack = [course for course, count in enumerate(num_prereqs) if count == 0]
+        completed = 0
+
+        while stack:
+            completed += 1
+            completed_course = stack.pop()
+            for post in prereqs[completed_course]:
+                num_prereqs[post] -= 1
+                if num_prereqs[post] == 0:
+                    stack.append(post)
+        
+        return completed == numCourses
+
+
+
